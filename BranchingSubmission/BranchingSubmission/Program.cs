e1129bbacc063fd64944e025783d89a22d277205
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-07 14:43:51 -0600

    Branching Assignment Complete

---
+++ BranchingSubmission/BranchingSubmission/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace BranchingSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introduction text
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            //Get weight of package
+            Console.WriteLine("Please enter the package weight:");
+            double pkgWeight = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
+            //Get width of package
+            Console.WriteLine("Please enter the package width:");
+            double pkgWidth = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
+            //Get height of package
+            Console.WriteLine("Please enter the package Height:");
+            double pkgHeight = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
+            //Get length of package
+            Console.WriteLine("Please enter the package Length:");
+            double pkgLength = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
+
+            if (pkgHeight + pkgLength + pkgWidth > 50) //Check if total of dimensions greater than 50, if so end program with message
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+            }
+            else //if not give price
+            {
+                double pkgCost = ((pkgWidth * pkgLength * pkgHeight) * pkgWeight) / 100; //calculate cost
+                Console.WriteLine("Your estimated total for shipping this package is: $" + pkgCost); // Give cost
+                Console.WriteLine("Thank you!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
