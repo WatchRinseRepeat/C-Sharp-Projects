d7f00cebf90d27ddcdc5a287abb7a7c6bfda839f
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-08 12:18:45 -0600

    Fix assignment step 3

--- BranchingSubmission/BranchingSubmission/Program.cs
+++ BranchingSubmission/BranchingSubmission/Program.cs
@@ -12,28 +12,36 @@ namespace BranchingSubmission
             //Get weight of package
             Console.WriteLine("Please enter the package weight:");
             double pkgWeight = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
-            //Get width of package
-            Console.WriteLine("Please enter the package width:");
-            double pkgWidth = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
-            //Get height of package
-            Console.WriteLine("Please enter the package Height:");
-            double pkgHeight = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
-            //Get length of package
-            Console.WriteLine("Please enter the package Length:");
-            double pkgLength = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
+            if (pkgWeight > 50) {
+                Console.WriteLine("This package is too heavy to be shipped via package express. Have a good day.");
+                Console.ReadLine();
+            }
+            else
+            {
+                //Get width of package
+                Console.WriteLine("Please enter the package width:");
+                double pkgWidth = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
+                                                                         //Get height of package
+                Console.WriteLine("Please enter the package Height:");
+                double pkgHeight = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
+                                                                          //Get length of package
+                Console.WriteLine("Please enter the package Length:");
+                double pkgLength = Convert.ToDouble(Console.ReadLine());  // Convert the input from string to Double
 
-            if (pkgHeight + pkgLength + pkgWidth > 50) //Check if total of dimensions greater than 50, if so end program with message
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-                Console.ReadLine();
+                if (pkgHeight + pkgLength + pkgWidth > 50) //Check if total of dimensions greater than 50, if so end program with message
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                else //if not give price
+                {
+                    double pkgCost = ((pkgWidth * pkgLength * pkgHeight) * pkgWeight) / 100; //calculate cost
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + pkgCost); // Give cost
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
             }
-            else //if not give price
-            {
-                double pkgCost = ((pkgWidth * pkgLength * pkgHeight) * pkgWeight) / 100; //calculate cost
-                Console.WriteLine("Your estimated total for shipping this package is: $" + pkgCost); // Give cost
-                Console.WriteLine("Thank you!");
-                Console.ReadLine();
-            }
+            
         }
     }
 }
