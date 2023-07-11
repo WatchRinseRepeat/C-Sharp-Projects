56144a9a8cf1e5773f0413ed405ac4b139de0bb8
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-11 12:25:15 -0600

    Complete Void Method

---
+++ VoidMethod/VoidMethod/MathOp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class MathOp
+    {
+        public void ShowResult(int numOne, int numTwo)
+        {
+            numOne = numOne + numTwo; //Perform a math operation on parameter 1
+            Console.WriteLine(numTwo); //Display the second integer
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp mathOp = new MathOp();
+
+            mathOp.ShowResult(5, 10); //call the method passing in two numbers.
+
+            mathOp.ShowResult(numOne: 5, numTwo: 10); //call the method and name the parameters the arguments are being passed to
+
+            Console.ReadLine();
+        }
+    }
+}
