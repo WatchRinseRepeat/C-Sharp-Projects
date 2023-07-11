92baf6277a8b23a4064e202c03c8c2f55b1f8f44
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-11 14:48:06 -0600

    Complete Abstract Class Assignment

---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    abstract class Person
+    {
+        public string FirstName { get; set; } //string for first name
+        public string LastName { get; set; }  //string for last name
+
+        public virtual void SayName() //method to say the full name in the console
+        {
+            string fullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: " + fullName);
+        }
+    }
+
+}
