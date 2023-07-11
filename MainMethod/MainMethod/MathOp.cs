1620ecc52f16f1425569d763b3eea60c98b45cdf
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-11 11:14:29 -0600

    MainMethod Assignment Complete

---
+++ MainMethod/MainMethod/MathOp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class MathOp
+    {
+        public int Addition(int number)
+        {
+            return number + 5; //Add 5 to the int
+        }
+
+        public int Addition(double number)
+        {
+            return Convert.ToInt32(number + 5); //Add 5 to the double and then convert to Int
+        }
+
+        public int Addition(String number)
+        {
+            return Convert.ToInt32(number) + 5; //Convert string to int and then add 5
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp mathOp = new MathOp();
+            Console.WriteLine(mathOp.Addition(5));  //call the int version 
+            Console.WriteLine(mathOp.Addition(5.5)); //call the double version
+            Console.WriteLine(mathOp.Addition("5")); //call the string version
+
+            Console.ReadLine();
+        }
+    }
+}
