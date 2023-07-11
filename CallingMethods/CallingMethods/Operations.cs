df91db84c15683a3ab7b004af0ae1d73b3682e93
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-11 10:00:05 -0600

    Calling Methods Assignment Complete

---
+++ CallingMethods/CallingMethods/Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Operations
+    {
+        public int Addition(int integer)
+        {
+            return integer + 2;
+        }
+
+        public int Subtraction(int integer)
+        {
+            return integer - 2;
+        }
+
+        public int Multiplication(int integer)
+        {
+            return integer * 2;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer to do math operations on:"); // ask for integer
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Operations operations = new Operations(); //create new instance of the Operations class
+
+            Console.WriteLine(operations.Addition(input)); // call the Addition method
+            Console.WriteLine(operations.Subtraction(input)); //call the Subtraction method
+            Console.WriteLine(operations.Multiplication(input)); // call the Multiplication method
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
