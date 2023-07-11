8e04f89997ed35d0c1086e6d8de47d112eb1d370
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-11 15:13:37 -0600

    Complete Interface assignment

---
+++ Interface/Interface/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Employee : Person, IQuitablle
+    {
+
+        public override void SayName() //Overide the base method
+        {
+            Console.Write("Employee ");
+            base.SayName();
+        }
+
+        public void Quit() //implement the Quit method
+        {
+
+            Console.WriteLine(FirstName + " " + LastName + " quits!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuitablle employee = new Employee { FirstName = "Sample", LastName = "Student" }; //Instantiate employee using polymorphism
+
+            employee.Quit(); //call the method
+
+            Console.ReadLine();
+        }
+    }
+}
