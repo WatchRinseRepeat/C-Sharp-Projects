021caca7d5d0bd8865a2e31092cc01c58817c2dd
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-11 12:49:34 -0600

    Overload Class assignment

---
+++ OverloadClass/OverloadClass/Overload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadClass
+{
+    class Overload
+    {
+        public void Divide(int num1)
+        {
+            Console.WriteLine(num1 / 2); //Divide the input by 2 and send to teh Console.
+        }
+
+        public void Divide(string num1)  //Overload a method
+        {
+            Console.WriteLine(Convert.ToInt32(num1)/ 2); 
+        }
+
+        public static string Explain() //Create a method with output parameters AND Declare a class to be static
+        {
+            return ("This method took no parameters.");
+        }
+    }
+}
---
+++ OverloadClass/OverloadClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Overload overload = new Overload();  //Create instance of the object
+            
+            Console.WriteLine("Please enter an integer: "); //Ask user for an integer
+
+            overload.Divide(Console.ReadLine()); //Call method
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
