814d8e3c3aa394fbe126c8415fcbac719171a177
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-12 14:03:13 -0600

    Complete Override Operators Assignment

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator== (Employee one, Employee two) //Overload "is equal" to check if two employees are the same based on the ID property
+        {
+            if (one.ID == two.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator!= (Employee one, Employee two)//Overload "is not equal" to check if two employees are the same based on the ID property
+        {
+            if (one.ID == two.ID)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee { FirstName = "Al", LastName = "Smith", ID = 1 }; //instantiate employee 1
+            Employee emp2 = new Employee { FirstName = "Bob", LastName = "Smith", ID = 2 }; //instantiate employee 2
+
+            Console.WriteLine("Is Employee 1 equal to Employee 2?"); //write to consle
+            Console.WriteLine(emp1 == emp2); //report if emp1 is equal to emp2
+
+            Console.WriteLine("Is Employee 1 not equal to Employee 2?"); //write to console
+            Console.WriteLine(emp1 != emp2); //report if emp1 not equal to emp2
+
+            Console.ReadLine();
+        }
+    }
+}
