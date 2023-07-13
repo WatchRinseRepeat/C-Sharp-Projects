a1ec701bfd70798e2603edc38004f7599032db45
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-12 19:14:38 -0600

    Completed Struct assignment

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number(); //Instantiate number
+            number.Value = 5m; // Assigne value to Value in number
+
+            Console.WriteLine(number.Value); //Write to console
+            Console.ReadLine();
+        }
+    }
+}
