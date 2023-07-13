11889f8f000a0ee52c0e3717479e690843e5ded6
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-13 15:45:26 -0600

    Complete Constructor Assignment

---
+++ Constructor/Constructor/Example.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Example
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Example() : this(10) //constructor with no parameters
+        {
+            
+        }
+        public Example(int id) : this(id, "John Doe") //constructor with one parameter 
+        {
+            
+        }
+        public Example(int id, string name) //constructor with all parameters
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TWO = 2; //Create a constant.
+
+            var stringList = new List<String>(); //implicitly create variable
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
