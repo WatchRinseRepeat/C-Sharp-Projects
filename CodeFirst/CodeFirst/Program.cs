8fc2b9d3a17e9bb844f2f177d2a50838f466fb3b
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-07-15 20:15:42 -0600

    Challenge Assignment Complete

---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace CodeFirst { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { FirstName = "Bob", LastName = "Densley", Age = 13 };
+
+
+                ctx.Database.EnsureCreated();
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+
+    }
+
+
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(): base()
+        {
+
+        }
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Database=myDb;Trusted_Connection=true;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        
+
+        public DbSet<Student> Students { get; set;}
+
+    }
+
+}
